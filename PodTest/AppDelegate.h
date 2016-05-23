351887c37e6318952fd90ec8ba28bbfbf730ab7e
uzhuyaoyao <u_zhuyaoyao@yahoo.com>
2016-05-23 16:39:16 +0800

    add PodTest

---
+++ PodTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PodTest
+//
+//  Created by Mingdao on 16/5/23.
+//  Copyright © 2016年 Mingdao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
